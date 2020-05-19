d01a34b508dfbbf7f1fe9855b9a8b63e39a4aa8d
Álvaro Sánchez <forensor@hotmail.com>
2020-05-19 20:57:54 +0200

    fsdfsdf

--- ProyectoEntornos/ProyectoEntornos/Form1.Designer.cs
+++ ProyectoEntornos/ProyectoEntornos/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(348, 78);
+            this.label1.Location = new System.Drawing.Point(310, 82);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 0;
