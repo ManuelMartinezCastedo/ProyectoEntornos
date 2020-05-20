eb22d8cc2ad0e1756362eeafb88bf61ca7d919d2
Álvaro Sánchez <forensor@hotmail.com>
2020-05-20 22:01:47 +0200

    Añadida la lógica del botón

--- ProyectoEntornos/ProyectoEntornos/Form1.cs
+++ ProyectoEntornos/ProyectoEntornos/Form1.cs
@@ -16,5 +16,10 @@ namespace ProyectoEntornos
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tListaSocios.Text += tNombre.Text + tApellidos.Text + tTelefono.Text + "\n";
+        }
     }
 }
